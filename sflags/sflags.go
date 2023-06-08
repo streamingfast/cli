7b80bbf03147360d466e5bb111ed60cdec1e3903
Matthieu Vachon <matt@streamingfast.io>
2023-06-08 10:01:45 -0400

    Added a way to retrieve viper bound key for a flag

--- sflags/sflags.go
+++ sflags/sflags.go
@@ -2,6 +2,7 @@ package sflags
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 )
 
 // Generate the flags based on Go code in this project directly, this however
@@ -13,4 +14,31 @@ import (
 // parent) and `false` otherwise.
 func FlagDefined(cmd *cobra.Command, name string) bool {
 	return cmd.Flags().Lookup(name) != nil
+}
+
+// MustGetViperKey returns the viper key associated with the flag `name` on the
+// `cmd` (or one of its parent). It panics if the flag is not defined.
+//
+// You must have used `cli.ConfigureViperForCommand(cmd, prefix)` or `cli.ConfigureViper(prefix)`
+// for this call to not panic.
+func MustGetViperKey(cmd *cobra.Command, name string) string {
+	flag := cmd.Flags().Lookup(name)
+	if flag == nil {
+		panic("flag not defined")
+	}
+
+	return MustGetViperKeyFromFlag(flag)
+}
+
+// MustGetViperKey returns the viper key associated with the flag.
+//
+// You must have used `cli.ConfigureViperForCommand(cmd, prefix)` or `cli.ConfigureViper(prefix)`
+// for this call to not panic.
+func MustGetViperKeyFromFlag(flag *pflag.Flag) string {
+	viperKey, ok := getReboundKey(flag)
+	if !ok {
+		panic("flag not rebound, are you sure you have called 'cli.ConfigureViperForCommand(cmd, prefix)' or used 'cli.ConfigureViper(prefix)' when building up your CLI?")
+	}
+
+	return viperKey
 }
